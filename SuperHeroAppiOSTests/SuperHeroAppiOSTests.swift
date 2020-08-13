6a9332e6853dd7e1330fd0e42772b8cd7d953d15
PablosFigueroa <figueroadpablos@gmail.com>
2020-08-12 19:01:13 -0500

    Initial Commit

---
+++ SuperHeroAppiOSTests/SuperHeroAppiOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  SuperHeroAppiOSTests.swift
+//  SuperHeroAppiOSTests
+//
+//  Created by Fernanda Fonseca on 8/12/20.
+//  Copyright © 2020 Pablo Figueroa. All rights reserved.
+//
+
+import XCTest
+@testable import SuperHeroAppiOS
+
+class SuperHeroAppiOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
